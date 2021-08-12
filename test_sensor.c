822e499b932bc929984e301edd5891e959d44ff4
M. Dietrich <mdt@emdete.de>
2021-08-12 14:25:20 +0200

    add animation to test_sensor

--- test_sensor.c
+++ test_sensor.c
@@ -1,19 +1,35 @@
-#include <avr/io.h>      // this contains all the IO port definitions
+#include <avr/io.h> // this contains all the IO port definitions
+#include <util/delay.h>
+
+void delay_ms( uint16_t milliseconds)
+{
+	for( ; milliseconds > 0; milliseconds--)
+	{
+		_delay_ms( 1);
+	}
+}
 
 
 int main(void) {
 
-  DDRB = 0xFF;        // set port B to output only
-  PORTB = 0;          // turn off all LEDs
+	DDRB = 0xFF; // set port B to output only
+	PORTB = 0; // turn off all LEDs
 
-  DDRD = 0xFB;        // one input on pin D2
-  PORTD = 0x04;       // turn on pullup on pin D2
+	DDRD = 0xFB; // one input on pin D2
+	PORTD = 0x04; // turn on pullup on pin D2
 
-  while (1) {
-    if (PIND & 0x4) {        // if the sensor switch is off...
-      PORTB = 0x0;
-    } else {                 // if the sensor switch is on...
-      PORTB = 0x1;
-    }
-  }
+	while (1) {
+		if (PIND & 0x4) { // if the sensor switch is off...
+			PORTB = 0xf0;
+		}
+		else { // if the sensor switch is on...
+			int s = 0x0f;
+			PORTB = s;
+			while (s != 0xf0) {
+				s <<= 1;
+				delay_ms(100);
+				PORTB = s;
+			}
+		}
+	}
 }
