dba4ea0230dbc93abaf498d76ae0d746d4b80c3c
M. Dietrich <mdt@emdete.de>
2021-08-16 10:40:32 +0200

    simplify code

--- alt_leds.c
+++ alt_leds.c
@@ -1,29 +1,13 @@
-#include <avr/io.h>      // this contains all the IO port definitions
-#include <util/delay.h>
-
-// This function basically wastes time
-void delay_ms( uint16_t milliseconds)
-{
-   for( ; milliseconds > 0; milliseconds--)
-   {
-      _delay_ms( 1);
-   }
-} 
-
+#include <avr/io.h> // this contains all the IO port definitions
+#include <util/delay.h> // this contains _delay_ms definition
 
 int main(void) {
-
-  DDRB = 0xFF;       // set all 8 pins on port B to outputs
-  
-  PORTB = 0x0;       // set all pins on port B low (turn off LEDs)
-
-  while (1) {
-    PORTB = 0xAA;      // turn on LEDs #2,4,6,8
-
-    delay_ms(500);    // delay half a second
-
-    PORTB = 0x55;      // turn on LEDs #1,3,5,7
-
-    delay_ms(500);    // delay half a second
-  }
+	DDRB = 0xFF; // set all 8 pins on port B to outputs
+	PORTB = 0x0; // set all pins on port B low (turn off LEDs)
+	while (1) {
+		PORTB = 0xAA; // turn on LEDs #2,4,6,8
+		_delay_ms(1000); // delay half a second
+		PORTB = 0x55; // turn on LEDs #1,3,5,7
+		_delay_ms(1000); // delay half a second
+	}
 }
